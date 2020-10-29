0744c12cefa4b620732e757984efda60fd04b5d6
TyMata <alvaromanuel.machado@correo.ucu.edu.uy>
2020-10-28 22:44:02 -0300

    Comentarios
    Se añaden comentarios y se cambia el nombre del metodo PrintRecipe() a PrintText()

--- src/Library/ITextToPrint.cs
+++ src/Library/ITextToPrint.cs
@@ -1,6 +1,13 @@
 
 namespace Full_GRASP_And_SOLID
 {
+    /*
+        Se desacoplo la clase de Recipe de la interfaz IPrinter y todas las clases que la implementan, 
+        de esta manera las clases dependen de esta interfaz (una abstraccion)
+        Recipe implementa esta interfaz y los printer reciben por parametro un objeto del tipo ITextToPrint
+        del cual utiliza la operacion GetTextToPrint para obtener el texto a imprimir.
+        Con estos cambios logramos reducir la rigidez, la fragilidad y la baja reutilización de nuestro programa.
+    */
     public interface ITextToPrint
     {
         string GetTextToPrint();
